4a5b7ef12218f9c27f65735a2460051d651c81ae
Pablo Corvera <pablocorvera9@gmail.com>
2025-03-31 15:46:37 -0300

    tp3

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -2,5 +2,21 @@ data Color = Azul | Rojo
 data Celda = Bolita Color Celda | CeldaVacia
 
 nroBolitas :: Color -> Celda -> Int
+nroBolitas _ CeldaVacia = 0
+nroBolitas colorBuscado (Bolita colorCelda cel) =
+  es1SiSino0 (elColorBuscadoEsIgualAlDeLaCelda colorBuscado colorCelda) + nroBolitas colorBuscado cel
 
+es1SiSino0 :: Bool -> Int
+es1SiSino0 True  = 1
+es1SiSino0 False = 0
 
+elColorBuscadoEsIgualAlDeLaCelda :: Color -> Color -> Bool
+elColorBuscadoEsIgualAlDeLaCelda cb cc = cb == cc
+
+poner :: Color -> Celda -> Celda
+poner colorAPoner CeldaVacia = Bolita colorAPoner CeldaVacia
+poner colorAPoner (Bolita cc c) = Bolita colorAPoner (Bolita cc c)
+
+sacar :: Color -> Celda -> Celda
+sacar colorASacar CeldaVacia = 
+sacar colorASacar (Bolita col c) =
